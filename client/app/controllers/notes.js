717cff87c084fd2276c8a7dc385fa0fd1d78dce1
Greg Bergé <neoziro@gmail.com>
2014-09-13 15:47:35 +0200

    Exercise 6: create note using a form.

--- client/app/controllers/notes.js
+++ client/app/controllers/notes.js
@@ -1,3 +1,14 @@
 window.notepad.controllers.controller('NotesCtrl', function ($scope, Note) {
+  $scope.createNote = function () {
+    $scope.notes.push($scope.note);
+    $scope.note.$save();
+    $scope.reset();
+  };
+
+  $scope.reset = function () {
+    $scope.note = new Note();
+  };
+
   $scope.notes = Note.query();
+  $scope.reset();
 });
